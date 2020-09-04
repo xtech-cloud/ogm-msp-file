1fa33c8e32c4f28885ac27bcf25630e3ae782211
Easlee Liu <easlee@outlook.com>
2020-09-04 13:36:36 +0000

    update

--- proto/file/bucket.pb.micro.go
+++ proto/file/bucket.pb.micro.go
@@ -36,12 +36,14 @@ var _ server.Option
 type BucketService interface {
 	// 创建一个存储桶
 	Make(ctx context.Context, in *BucketMakeRequest, opts ...client.CallOption) (*BlankResponse, error)
-	// 列举所有存储桶
-	List(ctx context.Context, in *BlankRequest, opts ...client.CallOption) (*BucketListResponse, error)
+	// 列举存储桶
+	List(ctx context.Context, in *BucketListRequest, opts ...client.CallOption) (*BucketListResponse, error)
 	// 删除一个存储桶
 	Remove(ctx context.Context, in *BucketRemoveRequest, opts ...client.CallOption) (*BlankResponse, error)
 	// 获取一个存储桶信息
 	Get(ctx context.Context, in *BucketGetRequest, opts ...client.CallOption) (*BucketGetResponse, error)
+	// 更新一个存储桶信息
+	Update(ctx context.Context, in *BucketUpdateRequest, opts ...client.CallOption) (*BlankResponse, error)
 }
 
 type bucketService struct {
@@ -66,7 +68,7 @@ func (c *bucketService) Make(ctx context.Context, in *BucketMakeRequest, opts ..
 	return out, nil
 }
 
-func (c *bucketService) List(ctx context.Context, in *BlankRequest, opts ...client.CallOption) (*BucketListResponse, error) {
+func (c *bucketService) List(ctx context.Context, in *BucketListRequest, opts ...client.CallOption) (*BucketListResponse, error) {
 	req := c.c.NewRequest(c.name, "Bucket.List", in)
 	out := new(BucketListResponse)
 	err := c.c.Call(ctx, req, out, opts...)
@@ -96,25 +98,38 @@ func (c *bucketService) Get(ctx context.Context, in *BucketGetRequest, opts ...c
 	return out, nil
 }
 
+func (c *bucketService) Update(ctx context.Context, in *BucketUpdateRequest, opts ...client.CallOption) (*BlankResponse, error) {
+	req := c.c.NewRequest(c.name, "Bucket.Update", in)
+	out := new(BlankResponse)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // Server API for Bucket service
 
 type BucketHandler interface {
 	// 创建一个存储桶
 	Make(context.Context, *BucketMakeRequest, *BlankResponse) error
-	// 列举所有存储桶
-	List(context.Context, *BlankRequest, *BucketListResponse) error
+	// 列举存储桶
+	List(context.Context, *BucketListRequest, *BucketListResponse) error
 	// 删除一个存储桶
 	Remove(context.Context, *BucketRemoveRequest, *BlankResponse) error
 	// 获取一个存储桶信息
 	Get(context.Context, *BucketGetRequest, *BucketGetResponse) error
+	// 更新一个存储桶信息
+	Update(context.Context, *BucketUpdateRequest, *BlankResponse) error
 }
 
 func RegisterBucketHandler(s server.Server, hdlr BucketHandler, opts ...server.HandlerOption) error {
 	type bucket interface {
 		Make(ctx context.Context, in *BucketMakeRequest, out *BlankResponse) error
-		List(ctx context.Context, in *BlankRequest, out *BucketListResponse) error
+		List(ctx context.Context, in *BucketListRequest, out *BucketListResponse) error
 		Remove(ctx context.Context, in *BucketRemoveRequest, out *BlankResponse) error
 		Get(ctx context.Context, in *BucketGetRequest, out *BucketGetResponse) error
+		Update(ctx context.Context, in *BucketUpdateRequest, out *BlankResponse) error
 	}
 	type Bucket struct {
 		bucket
@@ -131,7 +146,7 @@ func (h *bucketHandler) Make(ctx context.Context, in *BucketMakeRequest, out *Bl
 	return h.BucketHandler.Make(ctx, in, out)
 }
 
-func (h *bucketHandler) List(ctx context.Context, in *BlankRequest, out *BucketListResponse) error {
+func (h *bucketHandler) List(ctx context.Context, in *BucketListRequest, out *BucketListResponse) error {
 	return h.BucketHandler.List(ctx, in, out)
 }
 
@@ -141,4 +156,8 @@ func (h *bucketHandler) Remove(ctx context.Context, in *BucketRemoveRequest, out
 
 func (h *bucketHandler) Get(ctx context.Context, in *BucketGetRequest, out *BucketGetResponse) error {
 	return h.BucketHandler.Get(ctx, in, out)
+}
+
+func (h *bucketHandler) Update(ctx context.Context, in *BucketUpdateRequest, out *BlankResponse) error {
+	return h.BucketHandler.Update(ctx, in, out)
 }
